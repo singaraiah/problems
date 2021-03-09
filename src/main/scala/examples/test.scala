9a4da3ba321fef689c3de83143033f584a247c09
singaraiah <48911424+singaraiah@users.noreply.github.com>
2021-03-09 11:31:32 +0530

    Update test.scala

--- src/main/scala/examples/test.scala
+++ src/main/scala/examples/test.scala
@@ -3,6 +3,12 @@ package examples
 import scala.collection.mutable
 
 object test extends App {
+  case class Foo(n: Int)
+
+  val foo = Foo(1)
+
+  implicit def fooToInt(foo: Foo) = foo.n
+  println(foo + 1)
 
 }
 
