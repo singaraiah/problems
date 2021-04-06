6eb5bfd3058e9a867d493c73039470f36fbefed8
singaraiah <48911424+singaraiah@users.noreply.github.com>
2021-04-06 19:59:00 +0530

    refactoring

--- build.sbt
+++ build.sbt
@@ -6,10 +6,15 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
+val sparkVersion = "2.4.7"
+
 libraryDependencies ++=
-  Seq("org.apache.spark" %% "spark-core" % "2.4.2",
-    "org.apache.spark" %% "spark-sql" % "2.4.2",
+  Seq("org.apache.spark" %% "spark-core" % sparkVersion,
+    "org.apache.spark" %% "spark-sql" % sparkVersion,
+    "org.apache.spark" %% "spark-streaming" % sparkVersion,
     "org.apache.logging.log4j" % "log4j-core" % "2.11.1",
     "org.yaml" % "snakeyaml" % "1.25",
-    "org.elasticsearch" % "elasticsearch-hadoop" % "6.4.2"
+    "org.elasticsearch" % "elasticsearch-hadoop" % "6.4.2",
+    "org.scalatest" %% "scalatest" % "3.2.5" % "test",
+    "org.scalatestplus" %% "mockito-3-4" % "3.2.5.0" % "test"
   )
---
+++ src/test/scala/testing_playground/spark/TrySparkTest.scala
@@ -0,0 +1,43 @@
+package testing_playground.spark
+
+import org.apache.spark.sql.SparkSession
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.funsuite.AnyFunSuite
+import examples.spark.TrySpark.getTopRatedBooks
+
+class TrySparkTest extends AnyFunSuite with BeforeAndAfterEach {
+
+  var spark : SparkSession = _
+
+  override def beforeEach(): Unit = {
+    spark = SparkSession.builder()
+      .appName("unitTesting")
+      .master("local[2]")
+      .getOrCreate()
+  }
+
+  override def afterEach(): Unit = {
+    spark.stop()
+  }
+
+  test("testing getTopRatedBooks function") {
+    val sampleDF = spark.read.format("csv").option("header", "true")
+      .load("C:\\IdeaProjects\\CodeCoverage\\resource\\sample-books.csv")
+
+    val outDF = spark.read.format("csv").option("header", "true")
+      .load("C:\\IdeaProjects\\CodeCoverage\\resource\\sample-books-output.csv")
+
+    assert(getTopRatedBooks(sampleDF).schema.equals(outDF.schema) == true)
+
+    assert {
+      getTopRatedBooks(sampleDF)
+        .orderBy("bookId").collect()
+        .sameElements {
+          outDF.orderBy("bookId").collect()
+        } == true
+    }
+
+  }
+
+
+}
