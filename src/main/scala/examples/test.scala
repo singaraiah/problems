2b2f929f5067642c384e1573406f157bda629255
singaraiah <48911424+singaraiah@users.noreply.github.com>
2021-04-06 19:33:33 +0530

    new changes

--- src/main/scala/examples/test.scala
+++ src/main/scala/examples/test.scala
@@ -10,6 +10,17 @@ object test extends App {
   implicit def fooToInt(foo: Foo) = foo.n
   println(foo + 1)
 
+  println(List(1,2,4,5).map(_ > 3))
+
+  def sum(x: Int, y: Int) = x+y
+
+  def first(x: Int) = (y: Int) => x + y
+
+  val second = first(5)
+
+  def twice(op: Double => Double, x: Double) = op(op(x))
+
+  println(twice(_+1, 5))
 }
 
 
---
+++ src/main/scala/lec/coursera/Week3_1.scala
@@ -0,0 +1,18 @@
+package lec.coursera
+
+import com.carrotsearch.hppc.IntSet
+
+object Week3_1 {
+
+}
+
+abstract class InsSet{
+  def incl(x: Int): IntSet
+  def contains(x: Int): Boolean
+}
+
+class Empty extends InsSet{
+  override def contains(x: Int): Boolean = false
+
+  override def incl(x: Int): IntSet = ???
+}
